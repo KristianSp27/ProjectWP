0f70262bb1aafe96d875284bca2035f1d063ca2b
kristian <kristian.spasov27@gmail.com>
2024-02-07 21:36:57 +0200

    Correction

--- wp-content/themes/lazyfox/functions.php
+++ wp-content/themes/lazyfox/functions.php
@@ -17,9 +17,7 @@ function lazyfox_enqueue_assets() {
     
     wp_enqueue_style('bootstrap', $theme_dir_assets_url . '/css/bootstrap.min.css', array(), LAZYFOX_ASSETS_VERSION);
     
-    // <link rel="stylesheet" href="css/bootstrap.min.css">
-    // <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
-    // <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    
     // <link rel="stylesheet" href="css/ionicons.min.css">
     // <link rel="stylesheet" href="css/owl.carousel.css">
     // <link rel="stylesheet" href="css/owl.theme.css">
