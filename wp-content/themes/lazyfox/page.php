88811542f817bc490b01b9ef598cb3f1ed683f1f
kristian <kristian.spasov27@gmail.com>
2024-02-09 21:23:11 +0200

    Improving the dynamics.

--- wp-content/themes/lazyfox/page.php
+++ wp-content/themes/lazyfox/page.php
@@ -4,23 +4,74 @@
 
     <?php while( have_posts() ) : the_post(); ?>
 
-        <section id="product" <?php post_class( 'product' ) ?>>
-            <div class="container section-bg">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="title-box">
-                            <h2 class="title"><?php the_title(); ?></h2>
-                        </div>
-                    </div>
+    <section id="testimonial" class="testimonial-section padding-top-bottom-90 gradient-violat">
+      <div class="container">
+        <div class="heading-wraper text-center">
+          <h4 class="text-white">in the press</h4>
+          <hr class="heading-devider gradient-orange">
+        </div>
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div id="testimonial-carousel" class="owl-carousel">
+              <div>
+                <div class="testimonial-container">
+                  <div class="client-details text-center">
+                    <img src="<?php echo LAZYFOX_ASSETS_URL; ?>/images/t-1.png" alt="">
+                    <h5 class="client-name">Arix Wonder</h5>
+                    <p class="client-designation">CEO Of LVATO</p>
+                    <ul class="social-list">
+                      <li><a href=""><i class="ion-social-linkedin"></i></a></li>
+                      <li><a href=""><i class="ion-social-pinterest"></i></a></li>
+                      <li><a href=""><i class="ion-social-googleplus"></i></a></li>
+                    </ul>
+                  </div>
+                  <div class="testimonial-content">
+                    <p><i class="ion-quote"></i></p>
+                    <p class="testimonial-speech">Built using the latest web technologies like html5, css3, and jQuery, rest assured Sedna will look smashing on every device under the sun.Built using the latest web technologies like html5, css3, and jQuery.</p>
+                  </div>
                 </div>
-
-                <div class="row">
-                    <div class="col-sm-12">
-                    <div class="p-large"><?php the_content(); ?></div>
-                    </div>
+              </div>
+              <div>
+                <div class="testimonial-container">
+                  <div class="client-details text-center">
+                    <img src="<?php echo LAZYFOX_ASSETS_URL; ?>/images/t-1.png" alt="">
+                    <h5 class="client-name">Arix Wonder</h5>
+                    <p class="client-designation">CEO Of LVATO</p>
+                    <ul class="social-list">
+                      <li><a href=""><i class="ion-social-linkedin"></i></a></li>
+                      <li><a href=""><i class="ion-social-pinterest"></i></a></li>
+                      <li><a href=""><i class="ion-social-googleplus"></i></a></li>
+                    </ul>
+                  </div>
+                  <div class="testimonial-content">
+                    <p><i class="ion-quote"></i></p>
+                    <p class="testimonial-speech">Built using the latest web technologies like html5, css3, and jQuery, rest assured Sedna will look smashing on every device under the sun.Built using the latest web technologies like html5, css3, and jQuery.</p>
+                  </div>
                 </div>
-            </div>
-        </section>
+              </div>
+              <div>
+                <div class="testimonial-container">
+                  <div class="client-details text-center">
+                    <img src="<?php echo LAZYFOX_ASSETS_URL; ?>/images/t-1.png" alt="">
+                    <h5 class="client-name">Arix Wonder</h5>
+                    <p class="client-designation">CEO Of LVATO</p>
+                    <ul class="social-list">
+                      <li><a href=""><i class="ion-social-linkedin"></i></a></li>
+                      <li><a href=""><i class="ion-social-pinterest"></i></a></li>
+                      <li><a href=""><i class="ion-social-googleplus"></i></a></li>
+                    </ul>
+                  </div>
+                  <div class="testimonial-content">
+                    <p><i class="ion-quote"></i></p>
+                    <p class="testimonial-speech">Built using the latest web technologies like html5, css3, and jQuery, rest assured Sedna will look smashing on every device under the sun.Built using the latest web technologies like html5, css3, and jQuery.</p>
+                  </div>
+                </div>
+              </div>
+            </div>              
+          </div>
+        </div>
+      </div>
+    </section>
     <?php endwhile; ?>
 
 <?php endif; ?>
