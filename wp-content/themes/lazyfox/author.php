84c763291c96f43e6d5f01931278549fd6273191
kristian <kristian.spasov27@gmail.com>
2024-02-15 00:09:54 +0200

    Adding the authors' bios

--- wp-content/themes/lazyfox/author.php
+++ wp-content/themes/lazyfox/author.php
@@ -17,7 +17,6 @@
                             <div class="testimonial-container">
                                 <div class="client-details text-center">
                                     <img src="<?php echo LAZYFOX_ASSETS_URL; ?>/images/t-1.png" alt="">
-                                   
                                     <ul class="social-list">
                                         <li><a href=""><i class="ion-social-linkedin"></i></a></li>
                                         <li><a href=""><i class="ion-social-pinterest"></i></a></li>
@@ -27,6 +26,9 @@
                                 <div class="testimonial-content">
                                     <p><i class="ion-quote"></i></p>
                                     <?php the_content(); ?>
+                                </div>
+                                <div class="author-biography">
+                                    <p><?php echo 'Author: ' . get_the_author_meta('description'); ?></p>
                                 </div>
                             </div>
                         <?php
