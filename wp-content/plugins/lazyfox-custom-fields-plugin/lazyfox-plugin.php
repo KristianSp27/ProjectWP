b76540b929ef1aa72969d4c93141a0b515cdf34c
kristian <kristian.spasov27@gmail.com>
2024-02-14 23:33:34 +0200

    Begining the creation of my plugin

---
+++ wp-content/plugins/lazyfox-custom-fields-plugin/lazyfox-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: LazyFox Custom Plugin/ LazyFox Items
+ * Description: Custom plugin for the LazyFox project.
+ * Version: 1.0
+ * Author: Kristian Spasov
+ */
+
+include_once( plugin_dir_path( __FILE__ ) . 'includes/lazyfox-functions.php' );
